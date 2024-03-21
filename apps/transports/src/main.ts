ff469daaf370f2d5ca5a741a51ba1afb5f296bdd
Frank <00frankgarcia@gmail.com>
2024-03-21 01:30:22 -0300

    adding multiple configuration to transports microservice

--- apps/transports/src/main.ts
+++ apps/transports/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionFilter } from '@app/common';
+
 import { TransportsModule } from './transports.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(TransportsModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(TransportsModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.setGlobalPrefix('/api/v1');
+
+  await app.listen(process.env.PORT || 3000);
+  console.log("> Listening on port", Number(process.env.PORT || 3000));
 }
 bootstrap();
